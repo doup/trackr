4be5f2bf19710f8b4acac57f7ab0c7cd2243a6cc
doup <asier@illarra.com>
2015-04-22 00:46:24 +0200

    Window positioning + close via IPC

--- src/main.js
+++ src/main.js
@@ -1,63 +1,68 @@
 import app from 'app';
+import ipc from 'ipc';
 import BrowserWindow from 'browser-window';
 import CrashReporter from 'crash-reporter';
 import Tray from 'tray';
 
 import {Trackr} from './trackr.js';
 
-//
-// https://github.com/atom/electron/issues/752
-//
+const TRAY_ARROW_HEIGHT = 8; //px
+const WINDOW_WIDTH = 375;
 
 class GUI {
     constructor(trackr, app) {
+        var trayIcon = null;
+        var window = null;
+
         this.app    = app;
         this.trackr = trackr;
 
-        var appIcon = null;
-
-        // Keep a global reference of the window object, if you don't, the window will
-        // be closed automatically when the javascript object is GCed.
-        var mainWindow = null;
-
         this.app.dock.hide();
 
-        // Quit when all windows are closed.
         this.app.on('window-all-closed', () => {
             //if (process.platform != 'darwin') {
                 //this.app.quit();
             //}
         });
 
-        // This method will be called when atom-shell has done everything
-        // initialization and ready for creating browser windows.
         this.app.on('ready', () => {
-            appIcon = new Tray(__dirname + '/assets/tray-icon.png');
+            var screen = require('screen');
 
-            appIcon.on('clicked', () => {
-                mainWindow.show();
-            });
-
-            // Create the browser window.
-            mainWindow = new BrowserWindow({
-                width: 310,
+            window = new BrowserWindow({
+                width: WINDOW_WIDTH,
                 height: 335,
-                show: false,
+                title: 'Hello World',
                 resizable: false,
                 frame: false,
                 transparent: true,
+                show: false
             });
 
-            // and load the index.html of the app.
-            mainWindow.loadUrl('file://' + __dirname + '/index.html');
+            window.loadUrl('file://'+ __dirname +'/index.html');
 
-            // Emitted when the window is closed.
-            mainWindow.on('closed', () => {
-                // Dereference the window object, usually you would store windows
-                // in an array if your app supports multi windows, this is the time
-                // when you should delete the corresponding element.
-                // mainWindow = null;
+            window.on('close', () => {
+                window = null;
             });
+
+            trayIcon = new Tray(__dirname +'/assets/tray-icon.png');
+
+            trayIcon.on('clicked', () => {
+                // From: https://github.com/atom/electron/issues/752
+                var cursorPosition = screen.getCursorScreenPoint();
+
+                window.setPosition(cursorPosition.x - parseInt(WINDOW_WIDTH / 2), TRAY_ARROW_HEIGHT);
+
+                window.show();
+                window.focus();
+            });
+
+            window.on('blur', () => {
+                window.hide();
+            });
+        });
+
+        ipc.on('quit', (event) => {
+            app.quit();
         });
     }
 }

35c59832b9095b6d951d4338031c80d29e8ef602
doup <asier@illarra.com>
2015-07-29 21:56:28 +0200

    Remove

--- index.js
+++
@@ -1,8 +0,0 @@
-var Uptime = require('./dist/lib/trackr/uptime').Uptime;
-var uptime = new Uptime('/Users/doup/.trackr');
-
-uptime.stopChecking();
-
-var stats = uptime.getStats();
-console.log(JSON.stringify(stats, null, 4));
-console.log(stats.data.length);
